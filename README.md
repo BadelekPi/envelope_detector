90bce5c3c7c178f66a2149dd731c47a64a4e0705
badelekpi <diuf100@gmail.com>
2023-08-03 20:35:12 +0200

    README

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
+
+## Fourier transform
+<p align="center"><img width="640" height="480" src="doc/fourier_transform.jpg"></p>
+
+## References
 https://en.wikipedia.org/wiki/Envelope_detector
 
 https://stackoverflow.com/questions/32220053/how-to-find-envelope-of-uneven-waveform
--- envelope_detector.py
+++ envelope_detector.py
@@ -2,6 +2,7 @@
 import numpy as np
 import math
 import matplotlib.pyplot as plt
+from scipy.signal import hilbert, chirp
 
 # Carrier wave
 fc=10
@@ -20,6 +21,9 @@ m=Am/Ac
 # Modulated wave
 y=(1+m*ym)*0.1*yc
 
+analytic_signal = hilbert(y)
+amplitude_envelope = np.abs(analytic_signal)
 
-plt.plot(t, y)
+plt.plot(t, analytic_signal)
+plt.plot(t, amplitude_envelope)
 plt.show()
